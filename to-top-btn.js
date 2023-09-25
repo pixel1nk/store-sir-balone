0e6455aed891c53d30f12d047dec9b955f521a53
Juan Vasquez <juancvasdisenho@gmail.com>
2023-09-25 18:57:02 -0300

    prueba week 5

---
+++ to-top-btn.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+
+    const backToTop = $('#backToTop');
+    const amountScrolled = 300;
+  
+    $(window).scroll(() => {
+      $(window).scrollTop() >= amountScrolled ?
+      backToTop.fadeIn('fast') :
+      backToTop.fadeOut('fast');
+    });
+  
+    backToTop.click(() => {
+      $('body, html').animate({
+        scrollTop: 0 },
+      600);
+      return false;
+    });
+  });
