58f43b5cf13d8154b41582763d58b4d2cc045639
Juan Vasquez <juancvasdisenho@gmail.com>
2023-09-27 19:46:17 -0300

    ad final

---
+++ last.js
@@ -0,0 +1,13 @@
+document.write(`
+<div class="collection-items">
+        <div class="collection-item">
+            <h3>Data Speaks <br>(Free)</h3>
+            <a href="https://mail.sirbalone.com/data-speaks"><img src="https://sirbalone.com/img/data-speaks.png" alt="Weekly reports free suscription"></a>
+        </div>
+
+        <div class="collection-item">
+            <h3>Data Speaks Pro <br> Early Discount</h3>
+            <a href="https://mail.sirbalone.com/products/data-speaks-pro?promo=DATA10"><img src="https://sirbalone.com/img/data-speaks-pro.png" alt="Weekly reports premium suscription"></a>
+        </div>
+    </div>
+`);
