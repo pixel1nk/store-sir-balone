d4e25c17a2231da2069da02987727a6d5539ecda
Juan Vasquez <juancvasdisenho@gmail.com>
2023-09-27 19:36:46 -0300

    iconos header con script

--- header.js
+++ header.js
@@ -1,9 +1,9 @@
 document.write(`
-<div class="logo"><img src="https://sirbalone.com/sir-balone-logo.png" alt="Logo Sir Balone"></div>
+<div class="logo"><img src="https://sirbalone.com/img/sir-balone-logo.png" alt="Logo Sir Balone"></div>
         <div class="social-icons">
-            <a href="https://www.youtube.com/channel/UCLuVSIGxrTOp_0ZKiM1B-fA" target="_blank"><img src="https://sirbalone.com/youtube-icon.png" alt="YouTube"></a>
-            <a href="https://www.tiktok.com/@sirbalone" target="_blank"><img src="https://sirbalone.com/tiktok-icon.png" alt="TikTok"></a>
-            <a href="https://www.instagram.com/sir_balone" target="_blank"><img src="https://sirbalone.com/instagram-icon.png" alt="Instagram"></a>
-            <a href="https://sirbalone.com/es/inicio" target="_blank"><img src="https://sirbalone.com/es-lang.png" alt="Sitio en Español" title="Sitio en Español"></a>
+            <a href="https://www.youtube.com/channel/UCLuVSIGxrTOp_0ZKiM1B-fA" target="_blank"><img src="https://sirbalone.com/img/youtube-icon.png" alt="YouTube"></a>
+            <a href="https://www.tiktok.com/@sirbalone" target="_blank"><img src="https://sirbalone.com/img/tiktok-icon.png" alt="TikTok"></a>
+            <a href="https://www.instagram.com/sir_balone" target="_blank"><img src="https://sirbalone.com/img/instagram-icon.png" alt="Instagram"></a>
+            <a href="https://sirbalone.com/es/inicio" target="_blank"><img src="https://sirbalone.com/img/es-lang.png" alt="Sitio en Español" title="Sitio en Español"></a>
         </div>
 `);
