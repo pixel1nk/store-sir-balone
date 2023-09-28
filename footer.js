81187ef168852debc2d04d661678809721eae7a3
Juan Vasquez <juancvasdisenho@gmail.com>
2023-09-28 15:45:00 -0300

    final english

--- footer.js
+++ footer.js
@@ -7,8 +7,8 @@ document.write(`
         <a href="https://sirbalone.com/es/inicio" target="_blank"><img src="https://sirbalone.com/img/es-lang.png" alt="Sitio en Español" title="Sitio en Español"></a>
     </div>
     <div class="legal-links">
-        <a href="https://www.youtube.com/channel/UCLuVSIGxrTOp_0ZKiM1B-fA" target="_blank">Privacy Policy</a>
-        <a href="https://www.tiktok.com/@sirbalone" target="_blank">Copyright</a>
-        <a href="https://www.instagram.com/sir_balone" target="_blank">Contact</a>
+        <a href="https://sirbalone.com/privacy.html" target="_blank">Privacy Policy</a>
+        <a href="https://sirbalone.com/copyright.html" target="_blank">Copyright</a>
+        <a href="https://sirbalone.com/contact.html" target="_blank">Contact</a>
     </div>
 `);
