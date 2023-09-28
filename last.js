acfcccc4d1dbac2bee4b010b62835f623d63b411
Juan Vasquez <juancvasdisenho@gmail.com>
2023-09-28 16:01:13 -0300

    breadcrumbs to all

--- last.js
+++ last.js
@@ -3,7 +3,7 @@ document.write(`
 <hr>
 <div class="collection-items">
         <div class="collection-item">
-            <h3>Free Full Weekly Reports)</h3>
+            <h3>Free Full Weekly Reports</h3>
             <a href="https://mail.sirbalone.com/data-speaks"><img src="https://sirbalone.com/img/data-speaks.png" alt="Weekly reports free suscription"></a>
         </div>
 
