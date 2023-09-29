cfafe20cb94bb65eac4572ea17686b52ca4d9849
Juan Vasquez <juancvasdisenho@gmail.com>
2023-09-29 20:47:57 -0300

    optimizacion imagenes

--- footer.js
+++ footer.js
@@ -2,9 +2,9 @@ document.write(`
 <div class="logo"><img src="https://sirbalone.com/img/sir-balone-logo.webp" alt="Logo Sir Balone"></div>
     <div class="social-icons">
         <a href="https://www.youtube.com/channel/UCLuVSIGxrTOp_0ZKiM1B-fA" target="_blank"><img src="https://sirbalone.com/img/youtube-icon.png" alt="YouTube"></a>
-        <a href="https://www.tiktok.com/@sirbalone" target="_blank"><img src="https://sirbalone.com/img/tiktok-icon.png" alt="TikTok"></a>
+        <a href="https://www.tiktok.com/@sirbalone" target="_blank"><img src="https://sirbalone.com/img/tiktok-icon.webp" alt="TikTok"></a>
         <a href="https://www.instagram.com/sir_balone" target="_blank"><img src="https://sirbalone.com/img/instagram-icon.webp" alt="Instagram"></a>
-        <a href="https://sirbalone.com/es/inicio" target="_blank"><img src="https://sirbalone.com/img/es-lang.png" alt="Sitio en Español" title="Sitio en Español"></a>
+        <a href="https://sirbalone.com/es/inicio" target="_blank"><img src="https://sirbalone.com/img/es-lang.webp" alt="Sitio en Español" title="Sitio en Español"></a>
     </div>
     <div class="legal-links">
         <a href="https://sirbalone.com/privacy.html" target="_self">Privacy Policy</a>
--- header.js
+++ header.js
@@ -2,8 +2,8 @@ document.write(`
 <div class="logo"><a href="https://sirbalone.com"><img src="https://sirbalone.com/img/sir-balone-logo.webp" alt="Logo Sir Balone"></a></div>
         <div class="social-icons">
             <a href="https://www.youtube.com/channel/UCLuVSIGxrTOp_0ZKiM1B-fA" target="_blank"><img src="https://sirbalone.com/img/youtube-icon.png" alt="YouTube"></a>
-            <a href="https://www.tiktok.com/@sirbalone" target="_blank"><img src="https://sirbalone.com/img/tiktok-icon.png" alt="TikTok"></a>
+            <a href="https://www.tiktok.com/@sirbalone" target="_blank"><img src="https://sirbalone.com/img/tiktok-icon.webp" alt="TikTok"></a>
             <a href="https://www.instagram.com/sir_balone" target="_blank"><img src="https://sirbalone.com/img/instagram-icon.webp" alt="Instagram"></a>
-            <a href="https://sirbalone.com/es/inicio" target="_blank"><img src="https://sirbalone.com/img/es-lang.png" alt="Sitio en Español" title="Sitio en Español"></a>
+            <a href="https://sirbalone.com/es/inicio" target="_blank"><img src="https://sirbalone.com/img/es-lang.webp" alt="Sitio en Español" title="Sitio en Español"></a>
         </div>
 `);
