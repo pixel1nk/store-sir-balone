6f32ee73d532d3c08c73c2281ee86ea9131f7eff
Juan Vasquez <juancvasdisenho@gmail.com>
2023-09-29 20:35:53 -0300

    eu season 22-23 y webp

--- header.js
+++ header.js
@@ -1,9 +1,9 @@
 document.write(`
-<div class="logo"><a href="https://sirbalone.com"><img src="https://sirbalone.com/img/sir-balone-logo.png" alt="Logo Sir Balone"></a></div>
+<div class="logo"><a href="https://sirbalone.com"><img src="https://sirbalone.com/img/sir-balone-logo.webp" alt="Logo Sir Balone"></a></div>
         <div class="social-icons">
             <a href="https://www.youtube.com/channel/UCLuVSIGxrTOp_0ZKiM1B-fA" target="_blank"><img src="https://sirbalone.com/img/youtube-icon.png" alt="YouTube"></a>
             <a href="https://www.tiktok.com/@sirbalone" target="_blank"><img src="https://sirbalone.com/img/tiktok-icon.png" alt="TikTok"></a>
-            <a href="https://www.instagram.com/sir_balone" target="_blank"><img src="https://sirbalone.com/img/instagram-icon.png" alt="Instagram"></a>
+            <a href="https://www.instagram.com/sir_balone" target="_blank"><img src="https://sirbalone.com/img/instagram-icon.webp" alt="Instagram"></a>
             <a href="https://sirbalone.com/es/inicio" target="_blank"><img src="https://sirbalone.com/img/es-lang.png" alt="Sitio en Español" title="Sitio en Español"></a>
         </div>
 `);
