74b61c61d9eef90a5bc430e4f3f9abfa8cdc43cb
Juan Vasquez <juancvasdisenho@gmail.com>
2023-10-06 15:17:49 -0300

    git ignore y creacion refs

--- subscription.js
+++
@@ -1,5 +0,0 @@
-document.write(`
-<div class="barra-suscripcion">
-    <a href="https://mail.sirbalone.com" class="button2" target="_blank" rel="noopener">subscribe for free to get top 15 weekly reports</a>
-</div>
-`);
