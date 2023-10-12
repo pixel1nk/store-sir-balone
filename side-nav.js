00f82361930bab7d9a46495a3836300a7f29b69b
pixel1nk <juancvasdisenho@gmail.com>
2023-10-12 11:07:49 -0300

    balon de oro e imagenes redes

---
+++ side-nav.js
@@ -0,0 +1,24 @@
+const toggleButton = document.getElementById('toggle-nav');
+const navigation = document.getElementById('navigation');
+
+toggleButton.addEventListener('click', () => {
+  if (navigation.style.display === 'none' || getComputedStyle(navigation).display === 'none') {
+    navigation.style.display = 'block'; // Desplegar el panel
+    toggleButton.textContent = '◄'; // Cambiar el texto del botón
+  } else {
+    navigation.style.display = 'none'; // Minimizar el panel
+    toggleButton.textContent = '►'; // Cambiar el texto del botón
+  }
+});
+
+document.querySelectorAll('a[data-link]').forEach(link => {
+  link.addEventListener('click', (event) => {
+    if (!link.classList.contains('active-link')) {
+      
+      document.querySelectorAll('a[data-link]').forEach(otherLink => {
+        otherLink.classList.remove('active-link');
+      });
+      link.classList.add('active-link');
+    }
+  });
+});
