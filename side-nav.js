22a3f79a3d7e729d6b5a2f8fe502ed1bc7bf57e7
pixel1nk <juancvasdisenho@gmail.com>
2023-10-13 21:49:57 -0300

    nav

--- side-nav.js
+++ side-nav.js
@@ -4,17 +4,14 @@ const navigation = document.getElementById('navigation');
 toggleButton.addEventListener('click', () => {
   if (navigation.style.display === 'none' || getComputedStyle(navigation).display === 'none') {
     navigation.style.display = 'block'; // Desplegar el panel
-    toggleButton.textContent = '◄'; // Cambiar el texto del botón
   } else {
     navigation.style.display = 'none'; // Minimizar el panel
-    toggleButton.textContent = '<img src="https://sirbalone.com/img/menu-icon.svg" alt="navegacion vertical">'; // Cambiar el texto del botón
   }
 });
 
 document.querySelectorAll('a[data-link]').forEach(link => {
   link.addEventListener('click', (event) => {
     if (!link.classList.contains('active-link')) {
-      
       document.querySelectorAll('a[data-link]').forEach(otherLink => {
         otherLink.classList.remove('active-link');
       });
